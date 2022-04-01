411caeda2e31e31a3183e24fc2b3b8315305b59d
Souvik Karfa <94006990+Mrkarfa@users.noreply.github.com>
2022-04-02 01:58:17 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// add animation
+AOS.init();
+
+// nav hide
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
